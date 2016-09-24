d611a4d659d0dd427357e66c28052d64c1d07598
Azarel Howard <azariah001@gmail.com>
2016-09-24 23:30:16 +1000

    Made some changes.

--- configure-server.sh
+++ configure-server.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+if [$1 == ""] then;
+  echo "No input specified, please parse a domain, for example:
+
+  configure-server.sh example.com
+  "
+  exit
+fi
+
+
 if (($EUID != 0)); then
   if [[ -t 1 ]]; then
-    echo "Terribly sorry but in order to install nginx and letsencrypt and also make changes to /var/www, /etc/nginx, and /etc/letsencrypt we need sudo. If you're not comfortable with this please feel free to read the contents of this script, in fact you really should have already, just saying, I mean we could be scp copying the root of your system to some external server for all you know, we're not, but we could be. Always read the contents of scripts from unknown sources, especially if they ask for sudo like we are."
+    echo "
+    Terribly sorry but in order to install nginx and letsencrypt and also make changes to /var/www, /etc/nginx, and /etc/letsencrypt we need sudo. If you're not comfortable with this please feel free to read the contents of this script, in fact you really should have already, just saying, I mean we could be scp copying the root of your system to some external server for all you know, we're not, but we could be. Always read the contents of scripts from unknown sources, especially if they ask for sudo like we are.
+    "
     sudo "$0" "$@"
   else
     exec 1>output_file
@@ -12,13 +23,17 @@ if (($EUID != 0)); then
 fi
 
 
-echo "Before we can get started we're going to need a few things, namely nginx letsencrypt and ufw."
+echo "
+Before we can get started we're going to need a few things, namely nginx letsencrypt and ufw.
+"
 apt-get install nginx letsencrypt ufw
 
 
 ## Setup nginx config after backing up an existing file if it exists.
 if [ -e /etc/nginx/sites-available/$1 ]; then
-  echo "Backing up existing nginx config like a boss."
+  echo "
+  Backing up existing nginx config like a boss.
+  "
   mv /etc/nginx/sites-available/$1 /etc/nginx/sites-available/$1.bak
 fi
 
@@ -26,7 +41,9 @@ fi
 mkdir /var/www/$1
 
 ## Write a temporary nginx config just for let's encrypt.
-echo "Writing temporary nginx config file for Let's Encrypt Setup $1"
+echo "
+Writing temporary nginx config file for Let's Encrypt Setup $1
+"
 echo "
 server {
         listen 80;
@@ -53,25 +70,37 @@ nginx -t
 
 
 if [ ! -e /etc/nginx/sites-enabled/$1 ]; then
-  echo "Enabled this site is!"
+  echo "
+  Enabled this site is!
+  "
   ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
 else
-  echo "Check the backup config, you should! Already enabled this site was!"
+  echo "
+  Check the backup config, you should! Already enabled this site was!
+  "
 fi
 
-echo "Restarting nginx with the new configuration for letsencrypt."
+echo "
+Restarting nginx with the new configuration for letsencrypt.
+"
 service nginx restart
 
 ## Setup let's encrypt certs
-echo "We're now going to setup the letsencrypt certs, if this is the first time letsencrypt has been run on this machine it will prompt you for some input please be sure to fill out your details correctly as they will be needed should you ever need to recover or revoke ssl certs as part of damage mitigation."
+echo "
+We're now going to setup the letsencrypt certs, if this is the first time letsencrypt has been run on this machine it will prompt you for some input please be sure to fill out your details correctly as they will be needed should you ever need to recover or revoke ssl certs as part of damage mitigation.
+"
 
 letsencrypt certonly -a webroot --webroot-path=/var/www/$1 -d $1 -d $1
 
-echo "Should you need to backup your ssl certs for this site, or access them for any other reasons, they are located under /etc/letsencrypt/live/$1 You should see the files listed below."
+echo "
+Should you need to backup your ssl certs for this site, or access them for any other reasons, they are located under /etc/letsencrypt/live/$1 You should see the files listed below.
+"
 ls -l /etc/letsencrypt/live/$1
 
 mkdir /etc/nginx/snippets
-echo "Creating site specific ssl config file for easy inclusion in main configuration file. If you would like to edit any of the following config files manually they are located under /etc/nginx/snippets/"
+echo "
+Creating site specific ssl config file for easy inclusion in main configuration file. If you would like to edit any of the following config files manually they are located under /etc/nginx/snippets/
+"
 echo "
 ssl_certificate /etc/letsencrypt/live/$1/fullchain.pem;
 ssl_certificate_key /etc/letsencrypt/live/$1/privkey.pem;
@@ -79,11 +108,15 @@ ssl_certificate_key /etc/letsencrypt/live/$1/privkey.pem;
 
 
 if [ ! -e /etc/ssl/certs/dhparam.pem ]; then
-  echo "We are now going to generate a strong Diffie-Hellman group to strengthen the ssl encryption, this will take some time depending on your systems configuration."
+  echo "
+  We are now going to generate a strong Diffie-Hellman group to strengthen the ssl encryption, this will take some time depending on your systems configuration.
+  "
   openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 fi
 if [ ! -e /etc/nginx/snippets/ssl-params.conf ]; then
-  echo "We're also creating a config file for the Diffie-Hellman group, this is reusable so you won't see messages relating to Diffie-Hellman the next time you create a site on this server. Please note that there is a HSTS option that you may wish to configure in this file, ssl-params.conf"
+  echo "
+  We're also creating a config file for the Diffie-Hellman group, this is reusable so you won't see messages relating to Diffie-Hellman the next time you create a site on this server. Please note that there is a HSTS option that you may wish to configure in this file, ssl-params.conf
+  "
   echo "
   # from https://cipherli.st/
   # and https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html
@@ -112,7 +145,9 @@ fi
 
 ## Setup final nginx config 
 
-echo "Writing final nginx config file, this includes a redirect from http to https and configuration of the .well-known/ directory for the letsencrypt's automated renewal service which we'll setup in a minute and a proxy for forwarding traffic to a node.js instance running on a non-standard port."
+echo "
+Writing final nginx config file, this includes a redirect from http to https and configuration of the .well-known/ directory for the letsencrypt's automated renewal service which we'll setup in a minute and a proxy for forwarding traffic to a node.js instance running on a non-standard port.
+"
 echo "
 server {
     listen 80;
@@ -157,11 +192,14 @@ server {
 
 service nginx restart
 
-echo "Would you like to do a first time setup of UFW? [y/n]"
+echo "
+Would you like to do a first time setup of UFW? [y/n]"
 read prompt
-if [prompt == 'y'] then;
+if [prompt == 'y']; then
 
-  echo "Setting UFW rules to allow for inbound nginx traffic, we're also making sure that OpenSSH is allowed to ensure we don't lock you out of your server because 'Awkward that would be mrrrh?'"
+  echo "
+  Setting UFW rules to allow for inbound nginx traffic, we're also making sure that OpenSSH is allowed to ensure we don't lock you out of your server because 'Awkward that would be mrrrh?'
+  "
   ufw default deny incoming
   ufw default allow outgoing
   ufw delete allow 'Nginx HTTP'
@@ -171,10 +209,13 @@ if [prompt == 'y'] then;
   ufw enable
 
 else
+  
   ufw allow 'Nginx Full'
   ufw allow ssh
   ufw enable
+
 fi
+
 sudo ufw status
 
 echo "UFW is now enabled. If you need to configure it further, although we do recommend routing all incoming traffic through nginx, there's a comprehensive guide on UFW available here. https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-with-ufw-on-ubuntu-14-04"
